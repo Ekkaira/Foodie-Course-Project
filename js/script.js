95bbbc81ad2be697990590c81ed90522250e3712
ekkaira <denysxchebotar@gmail.com>
2023-01-31 08:34:50 +0200

    Used RegExp and optimised code

--- js/script.js
+++ js/script.js
@@ -359,11 +359,15 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  function strRegExp(str) {
+    return +str.replace(/\D/g, "");
+  }
+
   next.addEventListener("click", () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == strRegExp(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += strRegExp(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -380,9 +384,9 @@ window.addEventListener("DOMContentLoaded", function () {
 
   prev.addEventListener("click", () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = strRegExp(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= strRegExp(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -408,7 +412,7 @@ window.addEventListener("DOMContentLoaded", function () {
       const slideTo = e.target.getAttribute("data-slide-to");
 
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = strRegExp(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
